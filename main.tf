68aaec4dba54e730ba0022fd2ba008eb7401880d
Nathaniel Schweinberg <natanielks@gmail.com>
2019-08-29 14:03:41 -0500

    Update to be compatible with Terraform 0.12

--- main.tf
+++ main.tf
@@ -13,30 +13,31 @@ variable "output_path" {
 data "external" "packaging_script" {
   program = ["bash", "${path.module}/bin/package.sh"]
 
-  query {
-    source_dir = "${var.source_dir}"
-    build_dir  = "${var.build_dir}"
+  query = {
+    source_dir = var.source_dir
+    build_dir  = var.build_dir
   }
 }
 
 data "archive_file" "zip" {
   type        = "zip"
-  source_dir  = "${data.external.packaging_script.result.packaged_dir}"
-  output_path = "${var.output_path}"
+  source_dir  = data.external.packaging_script.result.packaged_dir
+  output_path = var.output_path
 }
 
 output "zip_file" {
-  value = "${var.output_path}"
+  value = var.output_path
 }
 
 output "output_sha" {
-  value = "${data.archive_file.zip.output_sha}"
+  value = data.archive_file.zip.output_sha
 }
 
 output "output_base64sha256" {
-  value = "${data.archive_file.zip.output_base64sha256}"
+  value = data.archive_file.zip.output_base64sha256
 }
 
 output "output_md5" {
-  value = "${data.archive_file.zip.output_md5}"
+  value = data.archive_file.zip.output_md5
 }
+
