f0a78646a579a5c522ef85d8143be39737331f77
Nathaniel Schweinberg <314009+nathanielks@users.noreply.github.com>
2020-04-14 11:44:54 -0500

    add logging and args file to facilitate debugging

--- bin/package.sh
+++ bin/package.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+JSON=$(jq -rc)
+SCRIPTNAME="$(basename ${0})"
+MD5="$(echo $JSON | md5sum | cut -d ' ' -f 1)"
+
+ARGSFILE="/tmp/$SCRIPTNAME-$MD5-args"
+LOGFILE="/tmp/$SCRIPTNAME-$MD5-logs"
+exec   > >(tee -ia $LOGFILE)
+exec  2> >(tee -ia $LOGFILE >& 2)
+exec 19> $LOGFILE
+
+export BASH_XTRACEFD="19"
+
+# Record args to file for debugging
+echo ${JSON} | tee $ARGSFILE
+
+function error_handler(){
+  echo "A fatal error occurred, closing..."
+  echo "Args file: $ARGSFILE"
+  >&2 cat "$LOGFILE"
+}
+
+trap error_handler ERR
 set -e
 
 if ! command -v jq >/dev/null 2>&1; then
@@ -17,7 +39,7 @@ if ! command -v deterministic-zip >/dev/null 2>&1; then
   exit 1
 fi
 
-eval "$(jq -r '@sh "SOURCE_DIR=\(.source_dir) BUILD_DIR=\(.build_dir) OUTPUT_PATH=\(.output_path)"')"
+eval "$(echo $JSON | jq -r '@sh "SOURCE_DIR=\(.source_dir) BUILD_DIR=\(.build_dir) OUTPUT_PATH=\(.output_path)"')"
 
 SOURCE_DIR="${SOURCE_DIR/#\~/$HOME}"
 BUILD_DIR="${BUILD_DIR/#\~/$HOME}"
